e4caf832347743ac7913297885eee9cfc2b628c9
Nukr <nukrs.w@gmail.com>
2015-06-10 10:18:10 +0800

    report table

--- client/src/js/views/Report/Report.react.js
+++ client/src/js/views/Report/Report.react.js
@@ -30,14 +30,33 @@ class Report extends Component {
     })
     return (
       <div style={{paddingTop: '60px'}}>
+        <table>
         <i className={faSpin}></i>
-        {
-          this.state.statistics.map(stat => {
-            return (
-              <div>{stat.billNo}</div>
-            )
-          })
-        }
+          {
+            this.state.statistics.map(stat => {
+              return (
+                <tr key={stat.billNo}>
+                  <td>{stat.billNo}</td>
+                  <td>{stat.creator}</td>
+                  <td>
+                    <ul>
+                      {
+                        stat.dishes.map((dish, index) => {
+                          return (
+                            <li key={`${stat.id}-dish${index}`}>{dish.name} - {dish.price} - {dish.quantity}</li>
+                          )
+                        })
+                      }
+                    </ul>
+                  </td>
+                  <td>{stat.shift === "morning" ? "早班" : "晚班"}</td>
+                  <td>{stat.credit ? "刷卡" : ""}</td>
+                  <td>{stat.discount ? "打折" : ""}</td>
+                </tr>
+              )
+            })
+          }
+        </table>
       </div>
     );
   }
